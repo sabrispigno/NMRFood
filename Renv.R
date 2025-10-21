55a72e824aa37d89b966b67a7b104f67bb0bf998
Sabrina Spigno <sabrina.spigno@unina.it>
2025-10-21 11:46:57 +0200

    Update Renv.R

--- Renv.R
+++ Renv.R
@@ -1,10 +1,25 @@
-#load packages
-library(renv)
+# #load packages
+# library(renv)
+# 
+# #initialize renv
+# renv::init()
+# 
+# #where are the packages
+# .libPaths()
+# renv::snapshot()
+# 
 
-#initialize renv
-renv::init()
+library(targets)
+use_targets()
+library(tarchetypes)
 
-#where are the packages
-.libPaths()
-renv::snapshot()
+renv::clean(prompt = FALSE)  # removes packages no longer referenced by renv.lock
+renv::status()
+# 1) Prune lockfile entries not required by your code
+renv::snapshot(prune = TRUE, prompt = FALSE)
 
+# 2) Remove packages from the project library that aren't in the lockfile
+renv::clean(prompt = FALSE)
+
+# 3) Re-check
+renv::status()
